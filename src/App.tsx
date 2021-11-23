a509864f4187189462064c432a81badd30c20761
TarasKlochko <60792921+TarasKlochko@users.noreply.github.com>
2021-11-23 14:04:22 +0200

    fix: change BrowserRouter for HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Employees from './pages/Employees/Employees';
 import './App.css';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Navigate replace to="/employees" />} />
         <Route path="employees" element={<Employees />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
